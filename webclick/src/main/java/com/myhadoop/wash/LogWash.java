c0c94ca2387583c0ee13cc053d4fb63082ba92af
lijinming <315768455@qq.com>
2018-01-11 22:31:34 +0800

    wash

--- webclick/src/main/java/com/myhadoop/wash/LogWash.java
+++ webclick/src/main/java/com/myhadoop/wash/LogWash.java
@@ -1,26 +1,67 @@
 package com.myhadoop.wash;
 
 import com.myhadoop.entity.WebLogBean;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LogWash {
 
     static class WashMapper extends Mapper<LongWritable, Text, WebLogBean, NullWritable> {
+        private Set<String> set = new HashSet<String>();
+        private WebLogBean webLogBean = new WebLogBean();
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            set.add("/about");
+            set.add("/black-ip-list/");
+            set.add("/cassandra-clustor/");
+            set.add("/finance-rhive-repurchase/");
+            set.add("/hadoop-family-roadmap/");
+            set.add("/hadoop-hive-intro/");
+            set.add("/hadoop-zookeeper-intro/");
+            set.add("/hadoop-mahout-roadmap/");
+            
+        }
+
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-
+             webLogBean = WebLogParserUtil.parserLog(value.toString());
+             webLogBean = WebLogParserUtil.filtStaticResource(webLogBean, set);
+             context.write(webLogBean,NullWritable.get());
 
         }
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(LogWash.class);
+        job.setMapperClass(WashMapper.class);
+        job.setOutputKeyClass(WebLogBean.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        job.setNumReduceTasks(0);
+
+
+        FileInputFormat.setInputPaths(job,new Path("G:/hadoop/webdata/data"));
+        FileOutputFormat.setOutputPath(job,new Path("G:/hadoop/webdata/out1"));
+        job.waitForCompletion(true);
+        System.exit(0);
 
     }
 }
--- webclick/src/main/java/com/myhadoop/wash/WebLogParserUtil.java
+++ webclick/src/main/java/com/myhadoop/wash/WebLogParserUtil.java
@@ -5,6 +5,7 @@ import com.myhadoop.entity.WebLogBean;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Set;
 
 public class WebLogParserUtil {
 
@@ -13,7 +14,29 @@ public class WebLogParserUtil {
         String[] fields = line.split(" ");
         WebLogBean webLogBean = new WebLogBean();
         if (fields.length > 11) {
+            webLogBean.setValid(true);
+            webLogBean.setRemote_addr(fields[0]);
+            webLogBean.setRemote_user(fields[1]);
+            webLogBean.setTime_local(parserDate(fields[3].substring(1)));
+            webLogBean.setRequest(fields[6]);
+            webLogBean.setStatus(fields[8]);
+            webLogBean.setBody_bytes_sent(fields[9]);
+            webLogBean.setHttp_referer(fields[10]);
 
+            if (fields.length>12) {
+                StringBuilder sb = new StringBuilder();
+                for(int i=11;i<fields.length;i++) {
+                    sb.append(fields[i]);
+                }
+                webLogBean.setHttp_user_agent(sb.toString());
+            }else {
+                webLogBean.setHttp_user_agent(fields[11]);
+            }
+            if (Integer.valueOf(webLogBean.getStatus())>=400) {
+                webLogBean.setValid(false);
+            }
+        }else {
+            webLogBean.setValid(false);
         }
 
 
@@ -32,5 +55,15 @@ public class WebLogParserUtil {
             e.printStackTrace();
         }
         return result;
+    }
+
+    //过滤静态资源
+    public static WebLogBean filtStaticResource (WebLogBean webLogBean, Set<String> set) {
+
+        if (!set.contains(webLogBean.getRequest())) {
+            webLogBean.setValid(false);
+        }
+
+        return webLogBean;
     }
 }
