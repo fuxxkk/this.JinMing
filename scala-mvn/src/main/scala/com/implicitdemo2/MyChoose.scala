a009db12048886552a93b32f018cd887a448abd1
lijinming <315768455@qq.com>
2018-02-26 20:37:17 +0800

    隐式转换

--- scala-mvn/src/main/scala/com/implicitdemo2/MyChoose.scala
+++ scala-mvn/src/main/scala/com/implicitdemo2/MyChoose.scala
@@ -1,15 +1,18 @@
 package com.implicitdemo2
 
-import MyPredef.richChoose
+import MyPredef.richoOrdingChoose
 
+//视图限定,T<%Ordered[T] 是一个隐式转换,将T转成Ordered类型
 class MyChoose[T<%Ordered[T]] {
   def choose(x:T,y:T):T={
     if(x>y) x else y
   }
 }
 
+//上下文限定,必须传进去一个隐式转换的值
 class MyChoose2[T:Ordering]{
   def c (x:T,y:T) : T={
+    //隐式转换的值
     val odr = implicitly[Ordering[T]]
     if(odr.gt(x,y)) x else y
   }
@@ -18,8 +21,10 @@ class MyChoose2[T:Ordering]{
 object MyChoose {
 
   def main(args: Array[String]): Unit = {
+
     val a = new Girl("aaa",90)
     val b = new Girl("bbb", 99)
+
     /*val choose = new MyChoose[Girl]
     val girl = choose.choose(a,b)
     println(girl.score)*/
--- scala-mvn/src/main/scala/com/implicitdemo2/MyPredef.scala
+++ scala-mvn/src/main/scala/com/implicitdemo2/MyPredef.scala
@@ -1,17 +1,18 @@
 package com.implicitdemo2
 
 object MyPredef {
+
   implicit def richChoose(g:Girl)= new Ordered[Girl]{
     override def compare(that: Girl): Int = {
       g.score-that.score
     }
   }
 
-  implicit def richoOrdingChoose(g:Girl)={
-    new Ordering[Girl]{
+  //用ordering要用函数形式
+  implicit val richoOrdingChoose= new Ordering[Girl]{
       override def compare(x: Girl, y: Girl): Int = {
         x.score-y.score
       }
     }
-  }
+
 }
